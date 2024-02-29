24785dd117ce79910d8512aeabc2b843f5b492f0
skillcrafter <olganigrova@yandex.ru>
2024-02-29 15:03:19 +0000

    NewLife-1.10

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -3,6 +3,7 @@ import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+APP_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 
