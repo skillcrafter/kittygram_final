7ac8c66707f6ce11f7d34cdeaab7dd8c6c265e8f
skillcrafter <olganigrova@yandex.ru>
2024-03-03 14:26:06 +0000

    NewLife-1.9.3

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -9,7 +9,8 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = os.getenv('DEBUG', False) == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='127.0.0.1, localhost').split(', ')
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
